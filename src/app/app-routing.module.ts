cdeac1ecb93c3dac28968d0a5e3d8c03bda4af6d
kappy <kapeeshmanilal@gmail.com>
2020-09-01 22:42:28 +0200

    lazy loading + a few components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core'
+import {Routes, RouterModule} from '@angular/router'
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: 'testimonials',
+    loadChildren: () => import('./testimonials/testimonials.module').then(m => m.TestimonialsModule)
+},
+    {
+        path: 'contact',
+        loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
+    },
+    {
+        path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
