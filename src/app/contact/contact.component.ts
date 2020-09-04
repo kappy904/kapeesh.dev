6e64eff189be22683d98860c27f0b52aee1edea2
Kappy-Lobster <46320707+Kappy-Lobster@users.noreply.github.com>
2020-09-04 16:32:28 +0200

    themable header

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core'
     styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+    titleColor = 'rgb(125 116 190 / 11%)'
     subtitleColor = '#7d74be'
 
     constructor() {
