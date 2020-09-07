9bd33ecc3a423169750243e47a48b50f346ba4e1
Kappy <>
2020-09-07 17:22:47 +0200

    add particle system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,74 +1,10 @@
-import { Component, OnInit } from '@angular/core'
-import * as p5 from 'p5'
+import {Component} from '@angular/core'
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-	title = 'kapeesh-dev'
-	private p5: any
-
-	ngOnInit(): void {
-		this.createCanvas()
-	}
-
-	private createCanvas() {
-		this.p5 = new p5(this.sketch)
-	}
-
-	private sketch(p: any) {
-		// original author greg smith
-		p.setup = () => {
-			let circles = [],
-				circle = {
-					x: undefined,
-					y: undefined,
-					r: undefined
-				},
-				overlapping = false,
-				NumCircles = 10,
-				protection = 100,
-				counter = 0,
-				canvasWidth = window.innerWidth,
-				canvasHeight = window.innerHeight
-
-
-			let cnv = p.createCanvas(canvasWidth, canvasHeight)
-			cnv.parent('globalCircles')
-
-			while (circles.length < NumCircles &&
-			counter < protection) {
-				circle = {
-					x: p.random(p.width),
-					y: p.random(p.height),
-					r: p.random(3, 36)
-				}
-				overlapping = false
-
-				for (let i = 0; i < circles.length; i++) {
-					let existing = circles[i]
-					let d = p.dist(circle.x, circle.y, existing.x, existing.y)
-					if (d < circle.r + existing.r) {
-						overlapping = true
-						break
-					}
-				}
-
-				if (!overlapping) {
-					circles.push(circle)
-				}
-
-				counter++
-			}
-
-			p.fill('#0a91651c')
-			p.noStroke()
-			for (let i = 0; i < circles.length; i++) {
-				p.ellipse(circles[i].x, circles[i].y,
-					circles[i].r * 2, circles[i].r * 2)
-			}
-		}
-	}
+export class AppComponent {
+  title = 'kapeesh-dev'
 }
--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-	selector: 'app-testimonials',
-	templateUrl: './testimonials.component.html',
-	styleUrls: ['./testimonials.component.scss']
+    selector: 'app-testimonials',
+    templateUrl: './testimonials.component.html',
+    styleUrls: ['./testimonials.component.scss'],
 })
-export class TestimonialsComponent {
-	titleColor = 'rgba(11, 149, 96, 0.11)'
-	subtitleColor = '#01935e'
+export class TestimonialsComponent implements OnInit {
+    titleColor = 'rgba(11, 149, 96, 0.11)'
+    subtitleColor = '#01935e'
 
-	constructor() {
-	}
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
 }
