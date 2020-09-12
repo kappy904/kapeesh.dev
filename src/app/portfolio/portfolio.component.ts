3db928ce61a4209a036c4caff83e09f747a996cf
kappy <kapeeshmanilal@gmail.com>
2020-09-12 20:20:48 +0200

    mobile fixes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,19 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
 	selector: 'app-portfolio',
 	templateUrl: './portfolio.component.html',
 	styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent {
 	titleColor = 'rgb(125 116 190 / 11%)'
 	subtitleColor = '#7d74be'
 	pageTitleColor: 'rgb(21, 0, 158)';
+	isVisible: boolean = true
+	@ViewChild('videoPlayer') videoplayer: ElementRef;
 
-	constructor() {
+	toggleVideo() {
+		this.isVisible = !this.isVisible
+		this.videoplayer.nativeElement.play();
 	}
-
-	ngOnInit(): void {
-	}
-
 }
