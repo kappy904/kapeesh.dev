633ffbb1591747ddca3c98532be4f3c47e950bb4
kappy <kapeeshmanilal@gmail.com>
2020-09-16 22:13:02 +0200

    JAMstack optimisations

--- _tailwind.config.js
+++ _tailwind.config.js
@@ -1,25 +1,26 @@
 require('dotenv').config();
 const enablePurge = process.env.ENABLE_PURGE || false;
 module.exports = {
-  purge: {
-    enabled: enablePurge,
-    content: [
-      './src/**/*.html',
-      './src/**/*.scss'
-    ]
-  },
-  theme: {
-    extend: {
-		colors: {
-			purple: '#7D74BE',
-			green: '#008521',
-			blue: '#15009E',
+	purge: {
+		enabled: enablePurge,
+		content: [
+			'./src/**/*.html',
+			'./src/**/*.scss'
+		]
+	},
+	theme: {
+		extend: {
+			colors: {
+				purple: '#7D74BE',
+				green: '#008521',
+				blue: '#15009E',
+			},
+			fontFamily: {
+				display: ['Merriweather Sans', 'sans-serif'],
+				body: ['Petrona', 'sans-serif'],
+			}
 		},
-      fontFamily: {
-        default: ['Merriweather Sans']
-      }
-    },
-  },
-  variants: {},
-  plugins: [],
+	},
+	variants: {},
+	plugins: [],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,27 @@
+/*
+require('dotenv').config();
+const enablePurge = process.env.ENABLE_PURGE || false;*/
 module.exports = {
+/*	purge: {
+		enabled: enablePurge,
+		content: [
+			'./src/!**!/!*.html',
+			'./src/!**!/!*.scss'
+		]
+	},*/
 	theme: {
-		fontFamily: {
-			display: ['Merriweather Sans', 'sans-serif'],
-			body: ['Petrona', 'sans-serif'],
+		extend: {
+			colors: {
+				purple: '#7D74BE',
+				green: '#008521',
+				blue: '#15009E',
+			},
+			fontFamily: {
+				display: ['Merriweather Sans', 'sans-serif'],
+				body: ['Petrona', 'sans-serif'],
+			}
 		},
-
-		colors: {
-			purple: '#7D74BE',
-			green: '#008521',
-			blue: '#15009E',
-		}
-
-	}
+	},
+	variants: {},
+	plugins: [],
 }
-
