ca3f79473af8f32a26038cfe18a261f117a05666
kappy <kapeeshmanilal@gmail.com>
2020-09-25 19:57:30 +0200

    upgrade scully homepage animations desktop only

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/*
 require('dotenv').config();
-const enablePurge = process.env.ENABLE_PURGE || false;*/
+const enablePurge = process.env.ENABLE_PURGE || false;
 module.exports = {
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
 	purge: {
-		enabled: true,
+		enabled: enablePurge,
 		content: [
 			'./src/**/*.html',
 			'./src/!**/!*.scss'
