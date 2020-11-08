8d41875ee5aeaad9e496559cb0f233886eff479f
kappy <kapeeshmanilal@gmail.com>
2020-11-08 20:47:40 +0100

    revert

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core'
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router'
-import {HomeComponent} from './home/home.component'
-
 
 const routes: Routes = [
 	{ path: 'testimonials', loadChildren: () => import('./testimonials/testimonials.module').then(m => m.TestimonialsModule) },
 	{ path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) },
-	{ path: 'home', component: HomeComponent },
+	{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
 	{ path: 'portfolio', loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule) },
 	{ path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
 	{ path: 'cv', loadChildren: () => import('./cv/cv.module').then(m => m.CvModule) },
