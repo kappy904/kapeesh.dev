20aaa8d869f2ff4f430374ae7bead879ec3c7b1d
Kappy <>
2020-11-09 11:29:25 +0100

    add page animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
 import {Component} from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { fadeInAnimation } from './app.animations'
 
 @Component({
 	selector: 'app-root',
-	templateUrl: './app.component.html'
+	templateUrl: './app.component.html',
+	animations: [
+		fadeInAnimation
+	]
 })
-export class AppComponent {}
+export class AppComponent {
+	prepareRoute(outlet: RouterOutlet) {
+		return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+	}
+}
