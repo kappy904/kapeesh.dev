1849c97f627b634d3ad95f1796d1f538ab9b8b97
kappy <kapeeshmanilal@gmail.com>
2022-04-22 21:59:20 +0200

    about update

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,6 +10,9 @@ export class AboutComponent implements OnInit {
 	titleColor = 'rgb(125 116 190 / 11%)'
 	pageTitleColor = 'rgb(21, 0, 158)'
 	subtitleColor = '#5a1f9f'
+	yearsInAmsterdam = new Date().getFullYear() - 2019
+	miraayasAge = new Date().getMonth()
+	daiysAge = new Date().getFullYear() - 2018
 
   constructor(private _title: Title,private _meta: Meta) { }
 
